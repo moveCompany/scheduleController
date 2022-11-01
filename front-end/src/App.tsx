2a4fbaf9a51a7504dc247d04ab0f5b847f7d5e4d
harrysci <harrysci@naver.com>
2022-11-01 23:35:47 +0900

    routes 설정

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import MainPage from './pages/MainPage';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import CalenderPage from "./pages/CalenderPage";
+import LoginPage from "./pages/LoginPage";
+import RoutinesPage from "./pages/RoutinesPage";
+import BoardPage from "./pages/BoardPage";
 
 function App() {
   return (
-    <div>
-      <MainPage/>
-    </div>
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<CalenderPage/>}/>
+            <Route path="/routines" element={<RoutinesPage/>}/>
+            <Route path="/board" element={<BoardPage/>}/>
+            <Route path="/login" element={<LoginPage/>}/>
+        </Routes>
+    </BrowserRouter>
 
   )
 }
---
+++ front-end/src/pages/RoutinesPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const RoutinesPage = () => {
+  return (
+    <div>
+      일정 등록 페이지 입니다.
+    </div>
+  )
+};
+export default RoutinesPage;
